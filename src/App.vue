fb91ac3bd7f623a88ddc14b720b7446bc619965c
darioNauka <durbaniak00@wp.pl>
2025-01-12 22:10:53 +0100

    main12_01_2025

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
-  <div class="app">
-    <div class="nav">
-      <router-link to="/">spacerAAA</router-link>
-      <router-link to="/about">about</router-link>
-    </div>
-    <router-view />
+  <div class="wrapper">
+    <ClaimComponent />
+    <SearchInput />
   </div>
 </template>
-<style lang="scss">
+<script>
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+import ClaimComponent from './components/Claim.vue';
+import SearchInput from './components/Searchinput.vue';
+
+const API = 'https://images-api.nasa.gov/search';
+
+export default {
+  name: 'App',
+  components: {
+    ClaimComponent,
+    SearchInput,
+  },
+  data() {
+    return {
+      searchValue: '',
+      results: [],
+    };
+  },
+  methods: {
+    handleInput: debounce(function handleSearch() {
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500),
+  },
+};
+</script>
+<style lang="scss" scoped>
 @import url('https://fonts.googleapis,com/css?family=Montserrat:300,400,600,800');
 
 $font-weight-light: 300;
@@ -15,13 +45,28 @@ $font-weight-normal: 400;
 $font-weight-bold: 600;
 $font-weight-black: 800;
 
-  * {
+* {
     box-sizing: border-box;
   }
 
-  body {
-    font-family: 'Montserrat', sans-serif;
-    margin: 0;
-    padding: 0;
+body {
+  font-family: 'Montserrat', sans-serif;
+  margin: 0;
+  padding: 0;
   }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+    padding: 30px;
+    width: 100%;
+    height: 100vh;
+    background-image: url('./assets/heroimage.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 80% 0;
+}
 </style>
